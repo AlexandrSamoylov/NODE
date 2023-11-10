9e734da017b6bcb1d489169e6b619b31d594a5b2
= <aleksandrsamojlov27579@gmail.com>
2023-11-10 15:22:21 +0300

    rhrghmfgm

--- app.js
+++ app.js
@@ -1,27 +1,75 @@
-const path = require("path");
+const express = require("express");
+const favicon = require("express-favicon");
 const fs = require("fs");
 
-// fs.mkdir(path.join(__dirname, "tmp"), function (err) {
-//   if (err) {
-//     {
-//       console.log(err);
-//     }
-//     console.log("папка создана");
-//   }
-// });
+const ejs = require("ejs");
 
-const filepath = path.join(__dirname, "tmp", "2.txt");
-console.log(filepath);
-fs.writeFile(filepath, "segewrgherherth", function (err) {
-  if (err) {
-    console.log(err);
-  }
-  console.log("папка создана");
+const app = express();
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+const { addListener } = require("process");
+
+const port = "3000";
+
+const routeTest = "/test";
+const routeSlash = "/";
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "views")));
+app.use(favicon(__dirname + "/public/favicon.ico"));
+const filePath = path.join(__dirname, "tmp", "logger.txt");
+fs.writeFile(filePath, "", (err) => {
+  if (err) console.error(err);
+  console.log("файл создан");
 });
 
-fs.readFile(filepath, "utf-8", (err, data) => {
-  if (err) {
-    console.log(err);
-  }
-  console.log(data);
+app.get("/test", (req, res) => {});
+
+app.post("/test", (req, res) => {
+  addLine("Пинганули /test");
+  console.log("прошли по пути test");
+  res.end("прошли post test");
+});
+
+function addLine(line) {
+  line = line + " timestamp: " + new Date().toLocaleString();
+  fs.appendFile(
+    path.join(__dirname + "/tmp/logger.txt"),
+    line + "\n",
+    (err) => {
+      if (err) console.log(err);
+    }
+  );
+}
+
+//error handler
+app.use(function (req, res, next) {
+  const err = new Error("NO FOUND ERROR");
+  err.code = 404;
+  next(err);
+});
+
+//production error handler
+// console.log(app.get("env"));
+if (app.get("env") != "development") {
+  app.use(function (err, req, res, next) {
+    res.status = 404;
+    let silka =
+      "https://steamuserimages-a.akamaihd.net/ugc/856096098871732485/9AE061717B44506050E8D1AA5BAD3E51BCAD1185/?imw=1024&imh=768&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=true";
+    res.render("error", { err, silka });
+  });
+} else {
+  app.use(function (err, req, res, next) {
+    console.log(app.get("env"), err.code, err.message);
+  });
+}
+
+app.listen(port, function () {
+  console.log("Сервер запущен порт " + port);
+  addLine("Server started");
 });
