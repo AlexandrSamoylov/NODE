0ee8d39c632e4642690e9fc309e82410bb4be992
= <aleksandrsamojlov27579@gmail.com>
2023-12-08 11:43:10 +0300

    YRA CHTOTO RABOTAET

--- models/user.js
+++ models/user.js
@@ -1,7 +1,6 @@
-const { nextTick } = require("process");
-const sqlite = require("sqlite3").verbose();
+const sqlite3 = require("sqlite3").verbose();
+const bcrypt = require("bcrypt");
 const db = new sqlite3.Database("test.db");
-
 const sql =
   "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, email TEXT NOT NULL, password TEXT NOT NULL, age INTEGER NOT NULL)";
 
@@ -13,18 +12,10 @@ class User {
   static async create(dataForm, cb) {
     try {
       const salt = await bcrypt.genSalt(10);
-
       const hash = await bcrypt.hash(dataForm.password, salt);
       const sql1 =
-        "INSERT INTO users(name, email, password, age) Values(?, ?, ?, ?)";
-      db.run(
-        sql1,
-        dataForm.name,
-        dataForm.email,
-        dataForm.password,
-        dataForm.age,
-        cb
-      );
+        "INSERT INTO users (name, email, password, age) VALUES (?, ?, ?, ?)";
+      db.run(sql1, dataForm.name, dataForm.email, hash, dataForm.age, cb);
     } catch (err) {
       if (err) return next(err);
     }
@@ -34,12 +25,14 @@ class User {
     db.get("SELECT * FROM users WHERE email = ?", email, cb);
   }
 
-  static authenticate(email, password, cb) {
+  static authentificate(dataForm, cb) {
     User.findByEmail(dataForm.email, (err, user) => {
       if (err) return cb(err);
       if (!user) return cb();
     });
     const result = bcrypt.compare(dataForm.password, user.password);
-    if (result) return user; // TODO: check
+    if (result) return cb(user); // TODO check
   }
 }
+
+module.exports = User;
--- routers/index_routers.js
+++ routers/index_routers.js
@@ -1,29 +1,16 @@
 const express = require("express");
 const router = express.Router();
+
 const register = require("../controllers/register");
+const entries = require("../controllers/entries");
 
-router.get("/", (req, res) => {
-  res.end("/");
-});
-router.post("/", function (req, res) {
-  res.end("/");
-});
-
-router.get("/entries", entries.form);
-router.post("/entry", entry.?);
+router.get("/", entries.list);
+// router.post("/entry", entry.?);
 
 router.get("/register", register.form);
 router.post("/register", register.submit);
 
-router.get("/login", login.form);
-router.post("/login", login.submit);
-
-router.get("/test", function (req, res) {
-  res.end("/test");
-});
-router.post("/test", function (req, res) {
-  console.log("Прошли по пути post/test");
-  res.end("post/test");
-});
+// router.get("/login", login.form);
+// router.post("/login", login.submit);
 
 module.exports = router;
